0fe87ab0b4dfe25ab58c32b7d4919ae8fcc46947
AK望 <2497744746@qq.com>
2023-04-29 17:22:49 +0800

    creating models without validation

---
+++ models/creation_models_without_validation.py
@@ -0,0 +1,26 @@
+from pickle import INT
+from re import U
+from unittest.mock import Base
+from pydantic import BaseModel
+
+
+class User(BaseModel):
+    id: int
+    age: int
+    name: str = 'AKW'
+
+
+original_user = User(id=1, age=18)
+
+user_data = original_user.dict()
+print(user_data)
+
+fields_set = original_user.__fields_set__
+print(fields_set)
+
+new_user = User.construct(_fields_set=fields_set, **user_data)
+print(repr(new_user))
+print(new_user.__fields_set__)
+
+bad_user = User.construct(id='dog')
+print(repr(bad_user))
