0b5f10452e2f942fbb32e8851b8ec174347b31bf
AK望 <2497744746@qq.com>
2023-05-03 12:13:10 +0800

    custom root types

---
+++ models/model_creation_from_namedtuple_or_typedict.py
@@ -0,0 +1,24 @@
+from os import name
+from typing_extensions import TypedDict
+
+from pydantic import ValidationError, create_model_from_typeddict
+
+
+class User(TypedDict):
+    name: str
+    id: int
+
+
+class Config:
+    extra = 'forbid'
+
+
+UserM = create_model_from_typeddict(User, __config__=Config)
+print(repr(UserM(name='AKW', id='3')))
+print(UserM(name='123', id=3))
+
+
+try:
+    UserM(name=123, id='3', other='no')
+except ValidationError as e:
+    print(e)
