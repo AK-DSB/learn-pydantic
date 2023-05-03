b2821c6b7fb29f8361aeda5280847ea808057760
AK望 <2497744746@qq.com>
2023-05-03 23:36:52 +0800

    custom root types

--- models/custom_root_types.py
+++ models/custom_root_types.py
@@ -1,5 +1,5 @@
 import json
-from pydantic import BaseModel
+from pydantic import BaseModel, ValidationError
 from pydantic.schema import schema
 
 
@@ -14,3 +14,17 @@ print(Pets.schema())
 
 pets_schema = schema([Pets])
 print(json.dumps(pets_schema, indent=2))
+
+print(Pets.parse_obj({'__root__': ['dog', 'cat']}))  # not recommended
+
+
+class PetsByName(BaseModel):
+    __root__: dict[str, str]
+
+
+print(PetsByName.parse_obj({'Otis': 'dog', 'Milo': 'cat'}))
+
+try:
+    PetsByName.parse_obj({'__root__': {'Otis': 'dog', 'Milo': 'cat'}})
+except ValidationError as e:
+    print(e)
