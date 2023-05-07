a11905cbdb0d7a353d24ad8b916786559d442592
AK望 <2497744746@qq.com>
2023-05-07 17:53:56 +0800

    abstract base classes

---
+++ models/abstract_base_classes.py
@@ -0,0 +1,30 @@
+import abc
+from pydantic import BaseModel, ValidationError
+
+
+class FooBarModel(BaseModel, abc.ABC):
+    a: str
+    b: int
+
+    @abc.abstractmethod
+    def my_abstract_method(self):
+        pass
+
+
+class Model(BaseModel):
+    a: int
+    b = 2
+    c: int = 1
+    d = 0
+    e: float
+
+
+print(Model.__fields__.keys())
+m = Model(e=2, a=1)
+print(m.dict())
+
+try:
+    Model(a='x', b='x', c='x', d='x', e='x')
+except ValidationError as e:
+    error_locations = [e['loc'] for e in e.errors()]
+    print(error_locations)
